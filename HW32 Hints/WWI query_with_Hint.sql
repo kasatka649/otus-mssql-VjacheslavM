9cefce1808e675389fd66aa887bb2fe70d05f068
@vjach812 <vjach@list.ru>
2024-08-12 10:33:18 +0300

    Hints

---
+++ HW32 Hints/WWI query_with_Hint.sql
@@ -0,0 +1,34 @@
+
+--Использование Hint со "старым" Legacy Cardinality Estimator оптимизирует запрос, на мой взгляд. (Скрины с "cost" прилагаются)
+
+
+--SET SHOWPLAN_ALL ON
+--GO
+
+Select ord.CustomerID, det.StockItemID, SUM(det.UnitPrice), SUM(det.Quantity), COUNT(ord.OrderID)
+FROM Sales.Orders AS ord
+JOIN Sales.OrderLines AS det
+ON det.OrderID = ord.OrderID
+JOIN Sales.Invoices AS Inv
+ON Inv.OrderID = ord.OrderID
+JOIN Sales.CustomerTransactions AS Trans
+ON Trans.InvoiceID = Inv.InvoiceID
+JOIN Warehouse.StockItemTransactions AS ItemTrans
+ON ItemTrans.StockItemID = det.StockItemID
+WHERE Inv.BillToCustomerID != ord.CustomerID
+AND (Select SupplierId
+FROM Warehouse.StockItems AS It
+Where It.StockItemID = det.StockItemID) = 12
+AND (SELECT SUM(Total.UnitPrice*Total.Quantity)
+FROM Sales.OrderLines AS Total
+Join Sales.Orders AS ordTotal
+On ordTotal.OrderID = Total.OrderID
+WHERE ordTotal.CustomerID = Inv.CustomerID) > 250000
+AND DATEDIFF(dd, Inv.InvoiceDate, ord.OrderDate) = 0
+GROUP BY ord.CustomerID, det.StockItemID
+ORDER BY ord.CustomerID, det.StockItemID
+OPTION (USE HINT ('FORCE_LEGACY_CARDINALITY_ESTIMATION')); --revert to the Legacy Cardinality Estimator
+GO 
+
+--SET SHOWPLAN_ALL OFF
+--GO
