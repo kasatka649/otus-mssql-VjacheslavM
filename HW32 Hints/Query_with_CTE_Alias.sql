e9df955186187df2aed8009a3c722f18f8f1d80b
@vjach812 <vjach@list.ru>
2024-08-13 12:04:02 +0300

    Create Query_with_CTE_Alias.sql

---
+++ HW32 Hints/Query_with_CTE_Alias.sql
@@ -0,0 +1,31 @@
+SET STATISTICS IO, TIME ON
+;WITH CTE (CustomerID, StockItemID,BillToCustomerID,UnitPrice,Quantity,OrderID) AS 
+(SELECT ord.CustomerID, det.StockItemID,inv.BillToCustomerID,det.UnitPrice,det.Quantity,ord.OrderID FROM Sales.Orders AS ord
+	JOIN Sales.OrderLines AS det
+ON det.OrderID = ord.OrderID
+	JOIN Sales.Invoices AS Inv
+ON Inv.OrderID = ord.OrderID
+	JOIN Sales.CustomerTransactions AS Trans
+ON Trans.InvoiceID = Inv.InvoiceID
+AND (SELECT SUM(Total.UnitPrice*Total.Quantity)
+FROM Sales.OrderLines AS Total
+	Join Sales.Orders AS ordTotal
+ON ordTotal.OrderID = Total.OrderID
+WHERE ordTotal.CustomerID = Inv.CustomerID) > 250000
+AND DATEDIFF(dd, Inv.InvoiceDate, ord.OrderDate) = 0
+)
+SELECT  CTE.CustomerID, 
+		CTE.StockItemID , 
+		SUM(CTE.UnitPrice) as SumPrice, 
+		SUM(CTE.Quantity) as SumQuantity, 
+		COUNT(CTE.OrderID) as CountOrder
+FROM CTE
+	JOIN Warehouse.StockItemTransactions AS ItemTrans
+ON ItemTrans.StockItemID = CTE.StockItemID
+WHERE CTE.BillToCustomerID != CTE.CustomerID
+AND (Select SupplierId
+FROM Warehouse.StockItems AS It
+Where It.StockItemID = CTE.StockItemID) = 12
+
+GROUP BY CTE.CustomerID, CTE.StockItemID
+ORDER BY CTE.CustomerID, CTE.StockItemID
